ec061de52159e5c48f763dbaaaef00151e3741a6
jcc1023 <original1023@gmail.com>
2025-05-15 09:48:09 +0900

    version control_A

--- chapter01/Version01.java
+++ chapter01/Version01.java
@@ -2,6 +2,6 @@ package chapter01;
 
 public class Version01 {
     public Version01(){
-        System.out.println(x:"tracked 파일");
+        System.out.println(x:"tracked 파일01");
     }
 }
--- chapter01/Version02.java
+++ chapter01/Version02.java
@@ -2,6 +2,6 @@ package chapter01;
 
 public class Version02 {
     public Version02(){
-        System.out.println(x:"Untracked 파일");
+        System.out.println(x:"Untracked 파일02");
     }
 }
