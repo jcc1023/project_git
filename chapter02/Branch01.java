073211af7e9a318bcc8ac4d6f31d5a72c780e415
jcc1023 <original1023@gmail.com>
2025-05-15 10:56:00 +0900

    branch test3_d

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -2,6 +2,7 @@ package chapter02;
 
 public class Branch01{
     public Branch01(){
-        System.out.println(x:"main에서 작업");
+        System.out.println(x:"master에서 작업");
+        System.out.println(x:"daum에서 작업");
     }
 }
